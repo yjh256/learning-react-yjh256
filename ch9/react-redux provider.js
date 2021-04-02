ef2778a0b685959d95360ee0f2e9341f100fafc0
yjh256 <yjh256@snu.ac.kr>
2021-04-02 13:34:24 +0900

    ch9 내용 정리

--- ch9/react-redux provider.js
+++ ch9/react-redux provider.js
@@ -9,3 +9,19 @@ const App = () =>
   </div>
 
 exports default App
+
+// Provider를 사용하는 예제
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import storeFactory from './store'
+
+const store = storeFactory()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-container')
+)
